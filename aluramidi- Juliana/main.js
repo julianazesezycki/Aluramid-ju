0efd55fd8abc8edfe35b8a9b0e9fe89c4706cdc7
julianazesezycki <126294088+julianazesezycki@users.noreply.github.com>
2023-10-26 21:44:51 -0300

    Add files via upload

--- aluramidi- Juliana/main.js
+++ aluramidi- Juliana/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-document. querySelector('#som_tecla_pom') .play();
+function tocaSom (idElementoAudio) {
+document. querySelector(idElementoAudio) .play();
   }
   
   const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,5 +8,9 @@ document. querySelector('#som_tecla_pom') .play();
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const
+    const idAudio = `#som_${instrumento} `;
+
+    tecla.onclick = function () {
+      
+    }
   }
